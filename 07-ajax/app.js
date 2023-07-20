317a84e3c0d054a459353490dfd63fec7983752e
ishika2203 <ishikabatra.net@gmail.com>
2023-07-20 12:48:48 +0530

    added a button

--- 07-ajax/app.js
+++ 07-ajax/app.js
@@ -1,18 +1,25 @@
-const xhr = new XMLHttpRequest();
-console.log(xhr);
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', () => {
+  getData();
+});
 
-xhr.open('GET', './api/sample.txt');
-xhr.onreadystatechange = function () {
+function getData() {
+  const xhr = new XMLHttpRequest();
   console.log(xhr);
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    const text = document.createElement('p');
-    text.textContent = xhr.responseText;
-    document.body.appendChild(text);
-  } else {
-    console.log({
-      status: xhr.status,
-      text: xhr.statusText,
-    });
-  }
-};
-xhr.send();
+
+  xhr.open('GET', './api/sample.txt');
+  xhr.onreadystatechange = function () {
+    console.log(xhr);
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const text = document.createElement('p');
+      text.textContent = xhr.responseText;
+      document.body.appendChild(text);
+    } else {
+      console.log({
+        status: xhr.status,
+        text: xhr.statusText,
+      });
+    }
+  };
+  xhr.send();
+}
